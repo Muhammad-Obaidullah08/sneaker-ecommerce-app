ba1cbb3fb31f98b5c26ca701e23afda03dd0a4d7
Muhammad Obaidullah <127332556+Muhammad-Obaidullah08@users.noreply.github.com>
2023-06-22 08:21:26 +0500

    completed hackathon ui project

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -13,21 +13,31 @@ const Page = () => {
                 <h1 className="font-bold text-3xl lg:text-4xl mt-10 uppercase text-sky-950">Contact Us</h1>
             </div>
 
-            <section className="p-2 mt-20 mb-40 mx-5 lg:mx-10">
+            <section className="p-2 mt-10 mb-40 mx-5 lg:mx-10">
                 <div>
-                    <Label htmlFor="name">Name</Label>
-                    <Input type="text" id="name" placeholder="Name" className="py-5" />
-                    <Label htmlFor="email">Email</Label>
-                    <Input type="email" id="email" placeholder="Email" className="py-5" />
-                    <Label htmlFor="number">Number</Label>
-                    <Input type="number" id="number" placeholder="Mobile Number" className="py-5" />
-                    <Label htmlFor="text">Address</Label>
-                    <Input type="text" id="address" placeholder="Home Address" className="py-5" />
+                        <div className="my-5">
+                            <Label htmlFor="name" className="ml-3 text-sky-950">Name</Label>
+                            <Input type="text" id="name" placeholder="Name"
+                                className="py-5" />
+                        </div>
+                        <div className="my-5">
+                            <Label htmlFor="email" className="ml-3 text-sky-950">Email</Label>
+                            <Input type="email" id="email" placeholder="Email" className="py-5" />
+                        </div>
+                        <div className="my-5">
+                            <Label htmlFor="number" className="ml-3 text-sky-950">Number</Label>
+                            <Input type="number" id="number" placeholder="Mobile Number" className="py-5" />
+                        </div>
+                        <div className="my-5">
+                            <Label htmlFor="text" className="ml-3 text-sky-950">Address</Label>
+                            <Input type="text" id="address" placeholder="Home Address" className="py-5" />
+                        </div>
                 </div>
 
                 <div className="flex justify-center items-center m-5">
                     <Button className="cursor-pointer bg-sky-950 hover:bg-sky-950 text-white hover:text-yellow-500 font-semibold">Submit</Button>
                 </div>
+
             </section>
 
             <Footer />
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,8 +1,15 @@
+"use client";
 import Image from "next/image"
+import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
 
 const Navbar = () => {
+
+    const [showMenu, setShowMenu]: any = useState(false)
+
+
+
     return (
         <>
             <nav className='bg-sky-950 flex items-center justify-around w-full h-16 sm:h-20'>
@@ -20,7 +27,7 @@ const Navbar = () => {
                 </ul>
                 <div className="flex justify-evenly items-center w-1/3 md:w-1/4">
                     <Image src="/images/cart-icon.png" alt="cart-icon" width={25} height={25} className="invert cursor-pointer md:h-8 w-8"></Image>
-                    <div className="hamburger md:hidden">
+                    <div className="hamburger md:hidden" onClick={() => setShowMenu(!showMenu)}>
                         <div className="line h-0.5 w-5 bg-white my-0.5"></div>
                         <div className="line h-0.5 w-5 bg-white my-0.5"></div>
                         <div className="line h-0.5 w-5 bg-white my-0.5"></div>
@@ -28,6 +35,17 @@ const Navbar = () => {
                     <Button variant="secondary" className="hidden sm:block cursor-pointer text-sky-950 font-semibold 2xl:text-lg 2xl:h-12">Sign In</Button>
                 </div>
             </nav>
+
+            <div className={showMenu ? "block" : "hidden"}>
+                <ul className=' flex-col md:hidden w-full items-start bg-sky-950'>
+                    <Link href="/home"><li className='border-t-2 border-t-slate-300 p-2 text-slate-300 text-sm xl:text-base  hover:text-white font-medium cursor-pointer w-full'>Home</li></Link>
+                    <Link href="/about"><li className='border-t-2 border-t-slate-300 p-2  text-slate-300 text-sm   hover:text-white font-medium cursor-pointer w-full'>About</li></Link>
+                    <Link href="/products"><li className='border-t-2 border-t-slate-300 p-2  text-slate-300 text-sm   hover:text-white font-medium cursor-pointer w-full'>
+                        Products</li></Link>
+                    <Link href="/contact"><li className='border-t-2 border-t-slate-300 border-b-2 border-b-slate-300 p-2 text-slate-300 text-sm  hover:text-white font-medium cursor-pointer w-full'>
+                        Contact</li></Link>
+                </ul>
+            </div>
         </>
     )
 }
